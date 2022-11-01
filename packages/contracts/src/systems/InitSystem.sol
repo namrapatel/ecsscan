fa389bf05d2f4587a4223e004a165e0d3b06f9ae
namrapatel <namrajpatel@gmail.com>
2022-11-01 19:41:53 -0400

    chore: add componentRead to deployscript

--- packages/contracts/src/systems/InitSystem.sol
+++ packages/contracts/src/systems/InitSystem.sol
@@ -10,10 +10,29 @@ import { defineExample } from "../prototypes/defineExample.sol";
 uint256 constant ID = uint256(keccak256("system.Init"));
 
 contract InitSystem is System {
-  constructor(IWorld _world, address _components) System(_world, _components) {}
+  uint256 counter;
+  mapping(uint256 => address) public readComponentIdToAddress;
+
+  constructor(
+    IWorld _world,
+    address _components,
+    uint256[] memory _readComponentsIds,
+    address[] memory readComponentsAddrs
+  ) System(_world, _components) {
+    require(
+      _readComponentsIds.length == readComponentsAddrs.length,
+      "InitSystem: readComponentsIds and readComponentsAddrs must have the same length"
+    );
+    counter = _readComponentsIds.length;
+    for (uint256 i = 0; i < counter; i++) {
+      readComponentIdToAddress[_readComponentsIds[i]] = readComponentsAddrs[i];
+    }
+  }
 
   function execute(bytes memory) public returns (bytes memory) {
     ExampleComponent exampleComponent = ExampleComponent(getAddressById(components, ExampleComponentID));
     defineExample(exampleComponent);
+
+    exampleComponent.set(ID, 25);
   }
 }
