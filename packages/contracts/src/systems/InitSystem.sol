ecb59ee35c847d1d9b7b3210ac05bc94774674b6
namrapatel <namrajpatel@gmail.com>
2022-11-03 11:51:34 -0400

    chore: add writeComponents to initsystem

--- packages/contracts/src/systems/InitSystem.sol
+++ packages/contracts/src/systems/InitSystem.sol
@@ -11,26 +11,40 @@ import { defineExample } from "../prototypes/defineExample.sol";
 uint256 constant ID = uint256(keccak256("system.Init"));
 
 contract InitSystem is System {
-  uint256 public counter;
+  uint256 public readCounter;
   uint256[] public readComponentIds;
   mapping(uint256 => address) public readComponentIdToAddress;
+
+  uint256 writeCounter;
+  uint256[] writeComponentIds;
+  mapping(uint256 => address) writeComponentIdToAddress;
 
   constructor(
     IWorld _world,
     address _components,
     uint256[] memory _readComponentsIds,
-    address[] memory _readComponentsAddrs
+    address[] memory _readComponentsAddrs,
+    uint256[] memory _writeComponentsIds,
+    address[] memory _writeComponentsAddrs
   ) System(_world, _components) {
     require(
       _readComponentsIds.length == _readComponentsAddrs.length,
       "InitSystem: readComponentsIds and readComponentsAddrs must have the same length"
     );
-    console.log("InitSystem: constructor");
-    console.log(_readComponentsIds.length);
-    counter = _readComponentsIds.length;
+    readCounter = _readComponentsIds.length;
     readComponentIds = _readComponentsIds;
-    for (uint256 i = 0; i < counter; i++) {
+    for (uint256 i = 0; i < readCounter; i++) {
       readComponentIdToAddress[_readComponentsIds[i]] = _readComponentsAddrs[i];
+    }
+
+    require(
+      _writeComponentsIds.length == _writeComponentsAddrs.length,
+      "InitSystem: writeComponentsIds and writeComponentsAddrs must have the same length"
+    );
+    writeCounter = _writeComponentsIds.length;
+    writeComponentIds = _writeComponentsIds;
+    for (uint256 i = 0; i < writeCounter; i++) {
+      writeComponentIdToAddress[_writeComponentsIds[i]] = _writeComponentsAddrs[i];
     }
   }
 
